752cbcd52238c9accd00647132ebc082f7311d52
Oleksiy Lazarenko <alexusunit@gmail.com>
2023-09-20 10:58:53 +0100

    feat: validate input for crud operations

--- run.py
+++ run.py
@@ -13,30 +13,38 @@ def log(*message):
     message = "\n".join(message)
     print(message)
 
-def confirm(yes, no):
-    option = input(yes).lower()
+def confirm(msg):
+    option = input(msg).lower()
     if option == "y" or option == "yes":
         return True
     elif option == "n" or option == "no":
-        print(no)
         return False
     else:
         print("Wrong input, please type 'y' or 'n'")
-        confirm()
+        confirm(msg)
 
-def validate_length(data, input_message, min_length, max_length):
-    if len(data.split(" ")) > 1:
-        print("Input must not contain spaces")
-        return validate_length(input(input_message), input_message, min_length, max_length)
+def validate_length(data, input_message, min_length, max_length, isSpaceProhibited = False):
+    if isSpaceProhibited:
+        if len(data.split(" ")) > 1:
+            print("Input must not contain spaces")
+            return validate_length(input(input_message), input_message, min_length, max_length, True)
     if len(data) < min_length:
         print(f"Input must be at least {min_length} characters long")
-        return validate_length(input(input_message), input_message, min_length, max_length)
+        return validate_length(input(input_message), input_message, min_length, max_length, True)
     elif len(data) > max_length:
         print(f"Input must be less than {max_length} characters long")
-        return validate_length(input(input_message), input_message, min_length, max_length)
+        return validate_length(input(input_message), input_message, min_length, max_length, True)
     else:
         return data
     
+def is_number(data, input_message):
+    try:
+        int(data)
+        return data
+    except ValueError:
+        print("Input must be a number")
+        return is_number(input(input_message), input_message)
+
 
 # main functions  
 def auth():
@@ -53,8 +61,8 @@ def auth():
                 print("Invalid username or password")
                 return False
         elif option == "2":
-            username = validate_length(input("Enter your username: "), "Enter your username: ", 2, 12)
-            password = validate_length(input("Enter your password: "), "Enter your password: ", 6, 12)
+            username = validate_length(input("Enter your username: "), "Enter your username: ", 2, 12, True)
+            password = validate_length(input("Enter your password: "), "Enter your password: ", 6, 12, True)
             if authGS.register(username, password):
                 log("Registration successful", f"Welcome {username}")
                 return True
@@ -68,11 +76,14 @@ def auth():
 
 def crud():
     while True:
-        log("Type '1' to create a new product", "Type '2' to read a product", "Type '3' to update a product", "Type '4' to delete a product", "Type '5' to go back to the main menu")
         log("1. Create a new product", "2. Read a product", "3. Update a product", "4. Delete a product", "5. Go Back")
         option = input("Enter your option: ")
         if option == "1":
-            isProductAdded = productListGS.add_product(input("Enter the product: "), input("Enter the calories: "))
+            if not confirm("Are you sure you want to add a new product? (y/n) or (yes/no): "):
+                continue
+            validatedProduct = validate_length(input("Enter the product: "), "Enter the product: ", 2, 20)
+            validatedCalories = is_number(input("Enter the calories: "), "Enter the calories: ")
+            isProductAdded = productListGS.add_product(validatedProduct, validatedCalories)
             if isProductAdded:
                 print("Product added successfully")
             else:
@@ -85,24 +96,57 @@ def crud():
             log("1. Update a product name", "2. Update a product calories", "3. Go Back")
             option = input("Enter your option: ")
             if option == "1":
-                product = input("Enter the product name to update: ")
-                new_product = input("Enter the new product name: ")
+                product = validate_length(input("Enter the product name to update: "), "Enter the product name to update: ", 2, 20)
+                products = productListGS.find_products_starting_with(product)
+                if len(products) == 0:
+                    print("Product not found")
+                    continue
+                elif len(products) > 1:
+                    print("Multiple products found, you must select one")
+                    for product in products:
+                        print(product[0] + ": " + product[1])
+                    continue
+                else:
+                    product = products[0][0]
+                    if not confirm(f"Are you sure you want to update a product name of {product}? (y/n) or (yes/no): "):
+                        continue
+                new_product = validate_length(input("Enter the new product name: "), "Enter the product name to update: ", 2, 20)
+                is_in_db = bool(productListGS.find_product(new_product))
+                if is_in_db:
+                    print(f"Product {new_product} already exists")
+                    continue
                 productListGS.update_products(product, new_product)
+                print("Product " + product + " updated to " + new_product)
             elif option == "2":
-                product = input("Enter the product to update: ")
-                new_calories = input("Enter the new calories: ")
-                productListGS.update_products_calories(product, new_calories)
+                product_input = validate_length(input("Enter the product name to update its calories: "), "Enter the product name to update its calories: ", 2, 20)
+                products = productListGS.find_products_starting_with(product_input)
+                product = productListGS.find_product(product_input)
+                if product:
+                    if not confirm(f"Are you sure you want to update the {product[1]} calories of {product[0]}? (y/n) or (yes/no): "):
+                        continue
+                elif len(products) == 0:
+                    print("Product not found")
+                    continue
+                elif len(products) > 1:
+                    print("Multiple products found")
+                    for product in products:
+                        print(product[0] + ": " + product[1])
+                    print("You must select one product")
+                    continue
+
+                new_calories = is_number(input("Enter the new calories: "), "Enter the new calories: ")
+                productListGS.update_products_calories(product[0], new_calories)
+                print("Calories of " + product[0] + " updated to " + new_calories + " calories")
             elif option == "3":
                 break
         elif option == "4":
             product = productListGS.find_product(input("Enter the product to delete: "))
             if product:
                 print(product[0] + ": " + product[1])
-                option = input("Are you sure you want to delete this product? (y/n): ")
-                if option == "y":
-                    productListGS.delete_product(product[0])
-                else:
-                    print("Product not deleted")
+                if not confirm(f"Are you sure you want to delete {product[0]}? (y/n) or (yes/no): "):
+                    continue
+                if productListGS.delete_product(product[0]):
+                    print(f"{product[0]} deleted")
         elif option == "5":
             break
         else:
