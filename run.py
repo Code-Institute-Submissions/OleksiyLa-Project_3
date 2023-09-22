5a27a9994751a27be6c1579493c1105513315511
Oleksiy Lazarenko <alexusunit@gmail.com>
2023-09-22 08:08:38 +0100

    refactor: import all options as one object insted of importing many functions

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-from googlesheet import googleSheetDB, authGS, productListGS
-from helpers_func import clear_terminal, log_exit_message, log, confirm, validate_length, is_number, prepare_string, select_option
-from options import login, register, add_new_product, read_product, update_product, delete_product, calculate_calories, set_calories_limit, update_password, delete_account, add_consumed_calories, get_consumed_calories, calculate_calories_limit, calculate_overall_progress, add_your_weight
+from googlesheet import authGS
+from helpers_func import clear_terminal, log_exit_message, log, select_option
+import options
 
 
 def auth():
@@ -10,7 +10,7 @@ def auth():
     """
     while True:
         log("Type '1' to login, '2' to register or '3' to exit", "Please select an option:", "1. Login", "2. Register", "3. Exit")
-        option = select_option(login, register)
+        option = select_option(options.login, options.register)
         if option == 'exit':
             print("Goodbye")
             break
@@ -25,7 +25,7 @@ def crud():
     clear_terminal()
     while True:
         log("1. Create a new product", "2. Read a product", "3. Update a product", "4. Delete a product", "5. Go Back")
-        option = select_option(add_new_product, read_product, update_product, delete_product)
+        option = select_option(options.add_new_product, options.read_product, options.update_product, options.delete_product)
         if option == 'exit':
             clear_terminal()
             break
@@ -38,8 +38,10 @@ def manage_personal_info():
     """
     clear_terminal()
     while True:
-        log("1. Change password", "2. Delete account", "3. Add calories consumed per today", "4. Set calories limit", "5. See calories consumed per today", "6. See calories limit", "7. See your progress", "8. Add your weight", "9. Go Back")
-        option = select_option(update_password, delete_account, add_consumed_calories, set_calories_limit, get_consumed_calories, calculate_calories_limit, calculate_overall_progress, add_your_weight)
+        log("1. Change password", "2. Delete account", "3. Add calories consumed per today", "4. Set calories limit", 
+            "5. See calories consumed per today", "6. See calories limit", "7. See your progress", "8. Add your weight", "9. Go Back")
+        option = select_option(options.update_password, options.delete_account, options.add_consumed_calories, options.set_calories_limit, 
+                               options.get_consumed_calories, options.calculate_calories_limit, options.calculate_overall_progress, options.add_your_weight)
         if option == 'exit':
             clear_terminal()
             break
@@ -55,7 +57,7 @@ def menu():
             log_exit_message(authGS.username)
             return
         log("1. CRUD calories table", "2. Calculate calories", "3. Personal info", "4. Exit")
-        option = select_option(crud, calculate_calories, manage_personal_info)
+        option = select_option(crud, options.calculate_calories, manage_personal_info)
         if option == 'exit':
             clear_terminal()
             log_exit_message(authGS.username)
