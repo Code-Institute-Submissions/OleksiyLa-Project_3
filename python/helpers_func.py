a03fff64f28912cd5a78ba062a17ba0a4c0a37fc
Oleksiy Lazarenko <alexusunit@gmail.com>
2023-09-26 12:46:20 +0100

    add \n for select option (deployment)

--- python/helpers_func.py
+++ python/helpers_func.py
@@ -173,8 +173,8 @@ def select_option(*options):
     """
     print("\n ")
     length = len(options)
-    option = check_option(is_number(input(" Select an option: "),
-                                    " Select an option: "), length)
+    option = check_option(is_number(input(" Select an option:\n"),
+                                    " Select an option:\n"), length)
     if int(option) == length:
         return 'exit'
     clear_terminal()
@@ -185,7 +185,7 @@ def enter_to_continue():
     """
     This function waits for the user to press enter
     """
-    input(f"\n Press {OK}'Enter'{Q} to continue...\n ")
+    input(f"\n Press {OK}'Enter'{Q} to continue...\n")
     clear_terminal()
 
 
